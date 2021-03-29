7e04f53a1b70826711eaad63d44b2ab87f2d6054
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-29 16:13:39 -0600

    heroku push

---
+++ endpoints.js
@@ -0,0 +1,221 @@
+something.heroku.com/api/diner/:id
+
+//GET diner (/api/diner/:id)
+{
+  userId,
+  username,
+  favoriteTrucks: [{
+    truckId,
+    truckName
+  }]
+}
+
+// [GET] operator (/api/operator/:id)
+{
+  userId,
+  username,
+  trucksOwned: [{
+    truckId,
+    truckName
+  }]
+}
+
+//[GET] individual truck (/api/trucks/:id)
+{
+  truckId,
+  truckName,
+  currentLocation,
+  imageOfTruck,
+  cuisineTypes: [cuisine names],
+  customerRatingAvg,
+  customerRatings: [{username, starratings}],
+  arrivalTime,
+  arrivalDate,
+  departureTime,
+  departureDate
+  menu: [{
+    itemName,
+    itemDescriptionm
+    itemPrice,
+    itemPhotoUrl
+  }]
+}
+
+// add menu table
+
+// [GET] all trucks (/api/trucks)
+
+[{
+  truckId,
+  truckName,
+  imageOfTruck
+  customerRatingAvg,
+  cuisineTypes
+}]
+
+// [POST] new diner/operator (/api/auth/new)
+//receive:
+
+{
+  username,
+  password,
+  role,
+  email
+}
+
+// return
+
+{
+  user_id,
+  user_name
+}
+
+// [POST] Login (/api/auth/login)
+//receive
+{
+  username,
+  password
+}
+
+return {
+  user_id,
+  username,
+  role,
+  email,
+  token  
+}
+
+// [POST] Add favorite truck(/api/diner/:id/favorites)
+//request
+{
+  truckId
+}
+
+//response 
+[{
+  truckId,
+  truckName
+}]
+
+//[POST] customer rating (/api/diner/:id/ratings)
+
+{
+  truckId,
+  rating
+}
+//response
+Ok
+
+//[POST] operator new truck (api/operator/:id/)
+{
+  truckName,
+  imageOfTruck,
+  cuisineTypes: [cuisine names]
+}
+
+//response 
+[{
+  truckId,
+  truckName
+}]
+
+// [POST] new menu item (api/operator/:id/:truckId/)
+{
+  itemName,
+  itemDescription,
+  itemPrice,
+  itemPhoto
+}
+
+// response
+
+[{
+  itemId,
+  itemName,
+  itemDescription,
+  itemPrice,
+  itemPhoto
+}]
+
+// [PUT] edit menu item (api/operator/:id/:truckId/:itemId)
+  {
+    itemName,
+    itemDescription,
+    itemPrice,
+    itemPhoto
+  }
+
+// response
+[{
+  itemId,
+  itemName,
+  itemDescription,
+  itemPrice,
+  itemPhoto
+}]
+
+// [PUT] edit truck info (api/operator/:id/:truckId/)
+{
+  truckName,
+  currentLocation,
+  imageOfTruck,
+  cuisineTypes: [cuisine names],
+  arrivalTime,
+  arrivalDate,
+  departureTime,
+  departureDate
+}
+
+// [PUT] edit diner rating of truck (/api/diner/:id/ratings)
+
+{
+  truckId,
+  rating
+}
+//response
+Ok
+
+// [DELETE] diner favorite truck (/api/diner/:id/favorites)
+{
+  truckId
+}
+
+//response
+[{
+  [{
+    truckId,
+    truckName
+  }]
+}]
+
+
+// [DELETE] truck (api/operator/:id/:truckId/)
+
+//response
+[{
+  truckId,
+  truckName
+}]
+
+// [DELETE] delete menu item (api/operator/:id/:truckId/:itemId)
+
+//response
+[{
+  itemId,
+  itemName,
+  itemDescription,
+  itemPrice,
+  itemPhoto
+}]
+
+// [DELETE]  diner rating of truck (/api/diner/:id/ratings)
+
+{
+  truckId,
+}
+//response
+Ok
+
+
+//![POST] diner search (/api/trucks/) 
+TBD
