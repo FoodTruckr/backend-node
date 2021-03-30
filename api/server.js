66476b08f37f27016dd5f0a0992f9a9c90e8f843
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-30 10:27:24 -0600

    added morgan

--- api/server.js
+++ api/server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+
+if (process.env.NODE_ENV === 'development') {
+  server.use(morgan('dev'))
+}
+
+// Catch all
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: `Sorry, this is not a valid location for a ${req.method} request`
+  })
+})
 
 module.exports = server
