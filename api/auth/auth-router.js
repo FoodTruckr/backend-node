7c1eee05f249faf389661979033ec84e41dec6e0
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-03-31 16:44:32 -0600

    user can create new user, and log in and get token

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,34 +1,49 @@
 const router = require('express').Router()
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 const { nanoid } = require('nanoid')
-const { jwtSecret, ROUNDS } = require('./secrets')
 const Auth = require('./auth-model')
+const { ROUNDS } = require('./secrets')
+
+const { checkUsernameExists, buildToken } = require('./auth-middleware')
 
 /***** Base URL: /api/auth *****/
 
+//! Need middleware to verify info from request
+
 // [POST] create new diner/operator (/new)
 router.post('/new', async (req, res, next) => {
-  const { username, password, role } = req.body
+  const { username, password, role, email } = req.body
   try {
     const hash = await bcrypt.hashSync(password, ROUNDS)
     const dbEntry = {
       username,
       password: hash,
       role,
+      email,
       user_external_id: nanoid(6)
     }
-    const newUser = await Auth.newUser(dbEntry)
-    res.status(201).json(newUser[0])
+    const newUser = await Auth.addUser(dbEntry)
+    res.status(201).json(newUser)
   } catch (err) {
     next(err)
   }
 })
 
 // [POST] Login (/login)
-router.post('/login', (req, res, next) => {
+router.post('/login', checkUsernameExists, (req, res, next) => {
   try {
-    res.send('exists')
+    if (bcrypt.compareSync(req.body.password, req.userExists.password)) {
+      const token = buildToken(req.userExists)
+      res.json({
+        userId: req.userExists.user_external_id,
+        username: req.userExists.username,
+        role: req.userExists.role,
+        email: req.userExists.email,
+        token
+      })
+    } else {
+      res.status(401).json({ message: 'Invalid Credentials' })
+    }
   } catch (err) {
     next(err)
   }
--- api/auth/secrets.js
+++ api/auth/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || 'SecretPassword',
-  ROUNDS: Number(process.env.BCRYPT_ROUNDS) || 8
+  ROUNDS: Number(process.env.BCRYPT_ROUNDS) || 10
 }
