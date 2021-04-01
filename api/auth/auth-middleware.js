1a6d3f8dd4b81d4bff195652b27b08f31e2bbe06
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-04-01 09:52:01 -0600

    restricted middleware added

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -28,7 +28,37 @@ const buildToken = (user) => {
   }
   return jwt.sign(payload, jwtSecret, config)
 }
+
+const restricted = (only) => (req, res, next) => {
+  const token = req.headers.authorization
+  if (!token) {
+    res.status(401).json({ message: 'A token is required ' })
+  } else {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        res
+          .status(401)
+          .json({ message: 'Token error, please try logging in again' })
+      } else {
+        req.decodedJwt = decoded
+        if (only && req.decodedJwt.role !== only) {
+          res.status(403).json({
+            message: `Sorry, only ${only}s are allowed here. Try a different account.`
+          })
+        } else if (!checkUsernameExists(req.decodedJwt.username)) {
+          res.status(404).json({
+            message:
+              "Something went wrong! We can't find you in our system! Please try logging in again."
+          })
+        } else {
+          next()
+        }
+      }
+    })
+  }
+}
 module.exports = {
   checkUsernameExists,
-  buildToken
+  buildToken,
+  restricted
 }
--- api/diner/diner-model.js
+++ api/diner/diner-model.js
@@ -1,5 +1,12 @@
 const db = require('../../data/db-config')
 
+async function userExists(username) {
+try {
+  return await db('users').where('username', username)
+} catch (err) {
+  next (err)
+}
+}
 
 
 module.exports {
